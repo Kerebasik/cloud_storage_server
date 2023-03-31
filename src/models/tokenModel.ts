4c80a0ab8a486021b0e5bd0f8e4a40904989376d
Кирилл <kirill.denisenko.01@gmail.com>
2023-03-31 13:30:26 +0300

    Created Schema and interface for refresh token and added RefreshToken model for project

---
+++ src/models/tokenModel.ts
@@ -0,0 +1,17 @@
+import mongoose, {Schema, model} from 'mongoose';
+import {MongooseType} from '../types/mongoose/mongoseType';
+
+mongoose.set('strictQuery', true);
+
+interface IRefreshToken {
+  _id:mongoose.Schema.Types.ObjectId;
+  user:mongoose.Schema.Types.ObjectId;
+  token:mongoose.Schema.Types.String;
+}
+
+const RefreshToken = new Schema<IRefreshToken>({
+  user:{type:MongooseType.ObjectId, ref:'User'},
+  token:{type: MongooseType.String, required:true}
+})
+
+export default model('RefreshToken', RefreshToken)
